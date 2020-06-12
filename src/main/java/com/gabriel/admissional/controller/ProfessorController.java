b83729eb2fac358ca130b8f284838ef0e79bd5f1
Gabriel Rodrigo <gabriel.rodrigo.mars@gmail.com>
2020-06-12 12:13:15 -0300

    Allow cors on professor rest controller

--- src/main/java/com/gabriel/admissional/controller/ProfessorController.java
+++ src/main/java/com/gabriel/admissional/controller/ProfessorController.java
@@ -3,6 +3,7 @@ package com.gabriel.admissional.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gabriel.admissional.model.entity.Professor;
 import com.gabriel.admissional.model.service.ProfessorService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class ProfessorController {
 	
